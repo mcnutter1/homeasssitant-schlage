21ba32dcba5e2663eace8695e1ccd19e4766c70b
Jason Learst <jason.learst@gmail.com>
2022-10-30 12:41:45 -0400

    remove deprecated DEVICE_CLASS, unused imports

--- custom_components/schlage/sensor.py
+++ custom_components/schlage/sensor.py
@@ -1,11 +1,9 @@
-
 import logging
-from typing import Any, Dict, Optional
+from homeassistant.components.sensor import SensorDeviceClass
 
 
 from homeassistant.helpers.entity import Entity
 
-from homeassistant.const import CONF_ID, CONF_TYPE, DEVICE_CLASS_BATTERY
 from homeassistant.core import callback
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.util import slugify
@@ -16,25 +14,19 @@ from .const import DOMAIN, TOPIC_UPDATE, BATTERY_ICON
 _LOGGER = logging.getLogger(__name__)
 
 
-
-
 async def async_setup_entry(hass, config_entry, async_add_entities):
-   
-
 
     SchlageAccount = hass.data[DOMAIN][config_entry.entry_id]
     devices = SchlageAccount.api.devices
     entities = []
     for device in devices:
-           entities.append(SchlageBatteryLifeSensor(device['name'], device['deviceId'], SchlageAccount))
+        entities.append(
+            SchlageBatteryLifeSensor(device["name"], device["deviceId"], SchlageAccount)
+        )
     async_add_entities(entities, True)
 
 
-
-
 class SchlageSensor(Entity):
-
-
     def __init__(self, name, device_id, schlage_device):
 
         self._name = name
@@ -42,7 +34,6 @@ class SchlageSensor(Entity):
         self._schlage_device = schlage_device
         self._state = None
         self._async_unsub_dispatcher_connect = None
-
 
     @property
     def should_poll(self) -> bool:
@@ -54,15 +45,14 @@ class SchlageSensor(Entity):
             "identifiers": {(DOMAIN, self._id)},
             "name": self._name,
             "manufacturer": "Schlage",
-            "model" : "Encode"
+            "model": "Encode",
         }
         return device_info
 
     async def async_update(self) -> None:
         _LOGGER.debug("%s -> self._state: %s", self._name, self._state)
+
     async def async_added_to_hass(self) -> None:
-
-
         @callback
         def update() -> None:
             self.async_schedule_update_ha_state(True)
@@ -84,18 +74,23 @@ class SchlageBatteryLifeSensor(SchlageSensor):
     @property
     def name(self) -> str:
         return "{} Battery Life Sensor".format(self._name)
+
     @property
     def unique_id(self) -> str:
         return slugify("{}_battery_life_sensor".format(self._id))
+
     @property
     def state(self) -> str:
         return self._schlage_device.api.states[self._id]["batteryLife"]
+
     @property
     def native_unit_of_measurement(self):
         return "%"
-    @property 
+
+    @property
     def device_class(self):
-        return DEVICE_CLASS_BATTERY
+        return SensorDeviceClass.BATTERY
+
     @property
     def icon(self):
-        return  BATTERY_ICON
+        return BATTERY_ICON
